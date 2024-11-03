5a64f4550cd894e7c9b6d43be47d01f40d693fe7
Armelrik <armelrik@hotmail.com>
2024-11-03 16:51:55 +0000

    goof

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -33,6 +33,6 @@ export async function GET() {
   }
 
   return NextResponse.redirect(
-    process.env.NODE_ENV === 'development' ? "http://localhost:3000" : "https://sharelife-one.vercel.app/"
+    process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://sharelife-one.vercel.app/"
   );
 }
